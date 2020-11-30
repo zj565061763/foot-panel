093d4a480c92f1efd8f63244126e501ef270c39f
zjun <565061763@qq.com>
2020-11-30 11:09:25 +0800

    demo

--- app/src/main/java/com/example/foot_panel/dialog/TestDialog.java
+++ app/src/main/java/com/example/foot_panel/dialog/TestDialog.java
@@ -1,16 +1,30 @@
 package com.example.foot_panel.dialog;
 
 import android.app.Activity;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 
-import com.example.foot_panel.R;
+import com.example.foot_panel.view.InputView;
 import com.sd.lib.dialoger.impl.FDialoger;
+import com.sd.lib.utils.context.FResUtil;
 
 public class TestDialog extends FDialoger
 {
+    private final InputView mInputView;
+    private final int mDefaultHeight = (int) (FResUtil.getScreenHeight() * 0.5f);
+
     public TestDialog(Activity activity)
     {
         super(activity);
+        setCanceledOnTouchOutside(false);
         setPadding(0, 0, 0, 0);
-        setContentView(R.layout.dialog_test);
+        setBackgroundDim(false);
+        setGravity(Gravity.BOTTOM);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+
+        mInputView = new InputView(activity, null);
+        mInputView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, mDefaultHeight));
+        setContentView(mInputView);
     }
 }
