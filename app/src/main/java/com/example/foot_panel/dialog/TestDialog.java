3521026bd40d6ffa9a8c5c6a58fc0e8fd3b43fd2
zjun <565061763@qq.com>
2020-12-02 17:00:42 +0800

    demo

--- app/src/main/java/com/example/foot_panel/dialog/TestDialog.java
+++ app/src/main/java/com/example/foot_panel/dialog/TestDialog.java
@@ -7,44 +7,46 @@ import android.view.WindowManager;
 
 import com.example.foot_panel.R;
 import com.sd.lib.dialoger.impl.FDialoger;
-import com.sd.lib.foot_panel.ext.FKeyboardListener;
+import com.sd.lib.foot_panel.ext.FWindowKeyboardListener;
 
 public class TestDialog extends FDialoger
 {
     private static final String TAG = TestDialog.class.getSimpleName();
+
+    private final FWindowKeyboardListener mKeyboardListener;
 
     public TestDialog(Activity activity)
     {
         super(activity);
         setCanceledOnTouchOutside(false);
         setPadding(0, 0, 0, 0);
-        setBackgroundDim(false);
         setGravity(Gravity.BOTTOM);
+
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
-
         setContentView(R.layout.dialog_test);
-    }
 
-    private final FKeyboardListener.Callback mKeyboardCallback = new FKeyboardListener.Callback()
-    {
-        @Override
-        public void onKeyboardHeightChanged(int height, FKeyboardListener listener)
+        mKeyboardListener = new FWindowKeyboardListener(activity)
         {
-            Log.i(TAG, "onKeyboardHeightChanged:" + height);
-        }
-    };
+            @Override
+            protected void onKeyboardHeightChanged(int height)
+            {
+                Log.i(TAG, "onKeyboardHeightChanged:" + height);
+            }
 
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-        FKeyboardListener.of(getOwnerActivity()).addCallback(mKeyboardCallback);
-    }
+            @Override
+            protected void onStart()
+            {
+                super.onStart();
+                Log.i(TAG, "onStart");
+            }
 
-    @Override
-    protected void onStop()
-    {
-        super.onStop();
-        FKeyboardListener.of(getOwnerActivity()).removeCallback(mKeyboardCallback);
+            @Override
+            protected void onStop()
+            {
+                super.onStop();
+                Log.i(TAG, "onStop");
+            }
+        };
+        mKeyboardListener.start(getWindow());
     }
 }
