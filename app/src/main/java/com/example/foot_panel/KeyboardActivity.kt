2112ae98d552696276e074076bee0d9869c28c2f
zjun <565061763@qq.com>
2021-06-17 21:33:56 +0800

    demo

--- app/src/main/java/com/example/foot_panel/KeyboardActivity.kt
+++ app/src/main/java/com/example/foot_panel/KeyboardActivity.kt
@@ -1,47 +1,37 @@
-package com.example.foot_panel;
+package com.example.foot_panel
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.foot_panel.databinding.ActivityKeyboardBinding;
-import com.example.foot_panel.dialog.TestDialog;
-import com.sd.lib.foot_panel.ext.FKeyboardListener;
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.foot_panel.databinding.ActivityKeyboardBinding
+import com.example.foot_panel.dialog.TestDialog
+import com.sd.lib.foot_panel.ext.FKeyboardListener
 
 /**
  * 软键盘监听
  */
-public class KeyboardActivity extends AppCompatActivity {
-    private static final String TAG = KeyboardActivity.class.getSimpleName();
-    private ActivityKeyboardBinding mBinding;
+class KeyboardActivity : AppCompatActivity(), View.OnClickListener {
+    private val TAG = KeyboardActivity::class.java.simpleName
+    private lateinit var _binding: ActivityKeyboardBinding
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mBinding = ActivityKeyboardBinding.inflate(getLayoutInflater());
-        setContentView(mBinding.getRoot());
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityKeyboardBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
 
-        /**
-         * 由于{@link FKeyboardListener}内部采用弱引用保存回调对象，所以这边回调对象要强引用
-         */
-        FKeyboardListener.of(this).addCallback(mCallback);
-
-        Log.i(TAG, "getCachedKeyboardVisibleHeight:" + FKeyboardListener.getCachedKeyboardVisibleHeight());
-
-        mBinding.btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new TestDialog(KeyboardActivity.this).show();
-            }
-        });
+        // 由于[FKeyboardListener]内部采用弱引用保存回调对象，所以这边回调对象要强引用
+        FKeyboardListener.of(this).addCallback(_callback)
+        Log.i(TAG, "getCachedKeyboardVisibleHeight:" + FKeyboardListener.getCachedKeyboardVisibleHeight())
     }
 
-    private final FKeyboardListener.Callback mCallback = new FKeyboardListener.Callback() {
-        @Override
-        public void onKeyboardHeightChanged(int height, FKeyboardListener listener) {
-            Log.i(TAG, "onKeyboardHeightChanged height:" + height);
+    private val _callback = FKeyboardListener.Callback { height, listener ->
+        Log.i(TAG, "onKeyboardHeightChanged height:${height}")
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            _binding.btn -> TestDialog(this@KeyboardActivity).show()
         }
-    };
+    }
 }
