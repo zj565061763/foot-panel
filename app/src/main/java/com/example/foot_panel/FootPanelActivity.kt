29a88a94307295b38b5772822eb5519b771f2355
zjun <565061763@qq.com>
2021-06-18 00:51:20 +0800

    demo

--- app/src/main/java/com/example/foot_panel/FootPanelActivity.kt
+++ app/src/main/java/com/example/foot_panel/FootPanelActivity.kt
@@ -1,19 +1,14 @@
-package com.example.foot_panel;
+package com.example.foot_panel
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.foot_panel.databinding.ActivityFootPanelBinding
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.foot_panel.databinding.ActivityFootPanelBinding;
-
-public class FootPanelActivity extends AppCompatActivity {
-    private static final String TAG = FootPanelActivity.class.getSimpleName();
-    private ActivityFootPanelBinding mBinding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mBinding = ActivityFootPanelBinding.inflate(getLayoutInflater());
-        setContentView(mBinding.getRoot());
+class FootPanelActivity : AppCompatActivity() {
+    private lateinit var _binding: ActivityFootPanelBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityFootPanelBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
     }
 }
