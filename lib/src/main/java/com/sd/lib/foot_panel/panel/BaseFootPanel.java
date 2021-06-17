c38976d051f67928fdc07c0e2d1a218f74a87121
zjun <565061763@qq.com>
2021-06-18 01:15:01 +0800

    BaseFootPanel

--- lib/src/main/java/com/sd/lib/foot_panel/panel/BaseFootPanel.java
+++ lib/src/main/java/com/sd/lib/foot_panel/panel/BaseFootPanel.java
@@ -3,27 +3,22 @@ package com.sd.lib.foot_panel.panel;
 import androidx.annotation.CallSuper;
 import androidx.annotation.NonNull;
 
-import java.lang.ref.WeakReference;
-
 public abstract class BaseFootPanel implements IFootPanel {
-    private WeakReference<HeightChangeCallback> mHeightChangeCallback;
+    private HeightChangeCallback mHeightChangeCallback;
 
     /**
      * 通知高度
      */
     protected final void notifyHeight(int height) {
-        final HeightChangeCallback callback = mHeightChangeCallback == null ? null : mHeightChangeCallback.get();
-        if (callback != null) {
-            callback.onHeightChanged(height, this);
-        } else {
-            releasePanel();
+        if (mHeightChangeCallback != null) {
+            mHeightChangeCallback.onHeightChanged(height, this);
         }
     }
 
     @CallSuper
     @Override
     public void initPanel(@NonNull HeightChangeCallback callback) {
-        mHeightChangeCallback = new WeakReference<>(callback);
+        mHeightChangeCallback = callback;
     }
 
     @CallSuper
