2a640b3ceafa65e05cfc1ee538f473dbf55ed03a
zjun <565061763@qq.com>
2021-06-18 09:46:56 +0800

    demo

--- app/src/main/java/com/sd/demo/foot_panel/HeightKeeperActivity.kt
+++ app/src/main/java/com/sd/demo/foot_panel/HeightKeeperActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.sd.lib.foot_panel.ext.FKeyboardHeightKeeper
 
 class HeightKeeperActivity : AppCompatActivity() {
-    private val _keyboardHeightKeeper by lazy { FKeyboardHeightKeeper(this) }
+    private val _keyboardHeightKeeper = FKeyboardHeightKeeper(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_height_keeper)
+
+        // 添加要保持键盘高度的View
         _keyboardHeightKeeper.addView(findViewById(R.id.tv_content))
     }
 }
