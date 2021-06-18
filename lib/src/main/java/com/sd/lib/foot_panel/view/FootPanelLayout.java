b7b72a22fe6de4e7eb22ac77aad5ebf2cc9f545f
zjun <565061763@qq.com>
2021-06-18 14:57:35 +0800

    FootPanelLayout

---
+++ lib/src/main/java/com/sd/lib/foot_panel/view/FootPanelLayout.java
@@ -0,0 +1,140 @@
+package com.sd.lib.foot_panel.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.sd.lib.foot_panel.FootPanelListener;
+import com.sd.lib.foot_panel.ext.FKeyboardHeightLayout;
+import com.sd.lib.foot_panel.panel.IFootPanel;
+import com.sd.lib.foot_panel.panel.KeyboardFootPanel;
+import com.sd.lib.foot_panel.panel.ViewFootPanel;
+
+public class FootPanelLayout extends FrameLayout {
+    private final Activity mActivity;
+
+    private final IFootPanel mKeyboardPanel;
+    private final IFootPanel mViewPanel;
+
+    private View mContentView;
+    private View mKeyBoardView;
+
+    private Callback mCallback;
+
+    public FootPanelLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        if (context instanceof Activity) {
+            mActivity = (Activity) context;
+        } else {
+            throw new IllegalArgumentException("context must be instance of " + Activity.class.getName());
+        }
+
+        mKeyboardPanel = new KeyboardFootPanel(mActivity);
+        mViewPanel = new ViewFootPanel(this);
+
+        mFootPanelListener.addFootPanel(mKeyboardPanel);
+        mFootPanelListener.addFootPanel(mViewPanel);
+    }
+
+    /**
+     * 设置回调对象
+     */
+    public void setCallback(Callback callback) {
+        mCallback = callback;
+    }
+
+    /**
+     * 设置内容View
+     */
+    public final void setContentView(View view) {
+        final View oldView = mContentView;
+        if (oldView == view) {
+            return;
+        }
+
+        mContentView = view;
+
+        if (oldView != null) {
+            removeView(oldView);
+        }
+
+        if (view != null) {
+            if (view.getParent() != FootPanelLayout.this) {
+                removeViewFromParent(view);
+            }
+            addView(view);
+
+            if (view != mKeyBoardView) {
+                mFootPanelListener.setCurrentFootPanel(mViewPanel);
+            }
+        } else {
+            mFootPanelListener.setCurrentFootPanel(null);
+        }
+    }
+
+    private View getKeyBoardView() {
+        if (mKeyBoardView == null) {
+            mKeyBoardView = new FKeyboardHeightLayout(mActivity);
+        }
+        return mKeyBoardView;
+    }
+
+    private final FootPanelListener mFootPanelListener = new FootPanelListener() {
+        @Override
+        protected void onFootHeightChanged(int height) {
+            if (mCallback != null) {
+                mCallback.onFootHeightChanged(height);
+            }
+        }
+
+        @Override
+        protected void onFootPanelChanged(@Nullable IFootPanel panel) {
+            super.onFootPanelChanged(panel);
+            if (panel == mKeyboardPanel) {
+                setContentView(getKeyBoardView());
+            }
+        }
+    };
+
+    @Override
+    public void onViewAdded(View child) {
+        super.onViewAdded(child);
+        if (child != mContentView) {
+            throw new RuntimeException("Illegal child:" + child);
+        }
+    }
+
+    @Override
+    public void onViewRemoved(View child) {
+        super.onViewRemoved(child);
+        if (child == mContentView) {
+            setContentView(null);
+        }
+    }
+
+    private static void removeViewFromParent(View view) {
+        if (view == null) {
+            return;
+        }
+
+        final ViewParent parent = view.getParent();
+        if (parent instanceof ViewGroup) {
+            try {
+                ((ViewGroup) parent).removeView(view);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public interface Callback {
+        void onFootHeightChanged(int height);
+    }
+}
